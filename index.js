ef23041957a0e7f2e77dd2ac59ebee78f9ce9328
lgpq <186059148+lgpq@users.noreply.github.com>
2025-02-22 20:35:40 +0900

    ログから日付を除去

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const http = require('node:http');
 const pug = require('pug');
 const server = http
   .createServer((req, res) => {
-    const now = new Date();
-    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
+    console.info(`Requested by ${req.socket.remoteAddress}`);
     res.writeHead(200, {
       'Content-Type': 'text/html; charset=utf-8'
     });
@@ -55,7 +54,7 @@ const server = http
             const decoded = decodeURIComponent(rawData);
             console.log(decoded);
             const answer = new URLSearchParams(decoded);
-            console.info(`[${now}] 投稿: ${decoded}`);
+            console.info(`投稿: ${decoded}`);
             res.write(
               `<!DOCTYPE html><html lang="ja"><body><h1>${answer.get('name')}さんは${answer.get('favorite')}に投票しました</h1></body></html>`
             );
@@ -71,10 +70,10 @@ const server = http
     }
   })
   .on('error', e => {
-    console.error(`[${new Date()}] Server Error`, e);
+    console.error(`Server Error`, e);
   })
   .on('clientError', e => {
-    console.error(`[${new Date()}] Client Error`, e);
+    console.error(`Client Error`, e);
   });
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
